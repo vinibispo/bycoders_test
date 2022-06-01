59953a6e8b708d736ca33ed491e96983acc780e6
vinibispo <vini.bispo015@gmail.com>
2022-06-01 00:49:47 -0300

    test(process): add test for process use case

---
+++ spec/models/cnab/process_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe Cnab::Process do
+  describe 'failures' do
+    it 'should return failure when there is no attributes' do
+      result = described_class.call
+
+      expect(result.failure?).to be true
+      expect(result.type).to eq(:invalid_attributes)
+    end
+  end
+
+  describe 'success' do
+    it 'should return processed data' do
+      cnab = "3201903010000014200096206760174753****3153153453JOÃO MACEDO   BAR DO JOÃO       "
+
+      result = described_class.call(cnab:)
+
+      expect(result.type).to eq(:cnab_processed)
+      expect(result.success?).to be true
+      expect(result.data[:cnab]).not_to be_nil
+    end
+  end
+end
